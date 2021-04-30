86b7d6e1ad5e5afae4e8938c6630d9d999d7ef84
haylen <haylen@foxmail.com>
2021-04-30 15:26:16 +0800

    add fuckrm.sh

---
+++ fuckrm.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+# 使用mv命令代替rm
+
+trashPath="${HOME}/.trash/"
+deleteDayBefore=180
+
+rm() {
+    date=$( date +'%Y-%m-%d' )
+    dir="${trashPath}${date}/"
+    if [[ ! -d ${dir} ]]; then
+        mkdir -p ${dir}
+    fi
+    removeExpiredDir 
+    declare -a params 
+    for p in $@; do
+        if [[ ${p[1]} != '-' ]]; then
+            params+=${p}
+        fi
+    done
+    mv ${params} ${dir}
+}
+
+removeExpiredDir() {
+    now=$( date +%s )
+
+    for d in $( ls ${trashPath} ); do
+        dTime=$( date -d ${d} +%s )
+        if [[ ($? == 0) && $(( (${now} - ${dTime}) / 86400)) -ge ${deleteDayBefore} ]]; then
+            /bin/rm -rf ${trashPath}${d}
+        fi
+    done
+}
